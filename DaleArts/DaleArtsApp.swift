d6253e129102ebcb5923c8becfeaa84d8f411bfc
bharat kumar <bk9735732777@gmail.com>
2023-02-09 08:38:16 +0530

    Initial Commit

---
+++ DaleArts/DaleArtsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaleArtsApp.swift
+//  DaleArts
+//
+//  Created by Bharat Kumar on 09/02/23.
+//
+
+import SwiftUI
+
+@main
+struct DaleArtsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
